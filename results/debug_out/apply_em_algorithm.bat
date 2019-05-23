8b09811a81123d747d343ba5c8817b106d113ffd
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2019-05-23 12:05:05 +0900

    change pass

--- results/debug_out/apply_em_algorithm.bat
+++ results/debug_out/apply_em_algorithm.bat
@@ -9,13 +9,13 @@ set BASE_PATH=%A%
 set EXE_PATH=%BASE_PATH%x64/Release/apply_em_algorithm.exe
 set INPUT_FEATURE_DIR=%BASE_PATH%data\preprocessed\debug
 set INPUT_ABD_MASK_DIR=%BASE_PATH%data\preprocessed\debug\mask
-set INPUT_PARAM_DIR=%BASE_PATH%results\debug\calc_init_val
-set OUTPUT_DIR=%BASE_PATH%results\debug\apply_em_algorithm
+set INPUT_PARAM_DIR=%BASE_PATH%results\debug_out\calc_init_val
+set OUTPUT_DIR=%BASE_PATH%results\debug_out\apply_em_algorithm
 set TEST_FILENAME_LIST_PATH=%BASE_PATH%data\preprocessed\debug\training_data_list.txt
 set FEATURE_NAME_LIST_PATH=%BASE_PATH%data\preprocessed\debug\feature_name_list.txt
 ::set INPUT_ATLAS_DIR=%BASE_PATH%results\generate_atlas
 
 %EXE_PATH% %INPUT_FEATURE_DIR% %INPUT_ABD_MASK_DIR% %INPUT_PARAM_DIR% ^
-            %OUTPUT_DIR% %TEST_FILENAME_LIST_PATH% %FEATURE_NAME_LIST_PATH% 
+            %OUTPUT_DIR% %TEST_FILENAME_LIST_PATH% %FEATURE_NAME_LIST_PATH%
 PAUSE
 exit
--- results/debug_out/apply_maximum_a_posteriori.bat
+++ results/debug_out/apply_maximum_a_posteriori.bat
@@ -9,16 +9,16 @@ set BASE_PATH=%A%
 set EXE_PATH=%BASE_PATH%x64/Release/apply_maximum_a_posteriori.exe
 set INPUT_FEATURE_DIR=%BASE_PATH%data\preprocessed\debug
 set INPUT_ABD_MASK_DIR=%BASE_PATH%data\preprocessed\debug\mask
-set INPUT_PARAM_DIR=%BASE_PATH%results\debug\apply_em_algorithm
+set INPUT_PARAM_DIR=%BASE_PATH%results\debug_out\apply_em_algorithm
 set INPUT_LABEL_DIR=%BASE_PATH%data\preprocessed\debug\label
-set OUTPUT_DIR=%BASE_PATH%results\debug\apply_maximum_a_posteriori
+set OUTPUT_DIR=%BASE_PATH%results\debug_out\apply_maximum_a_posteriori
 set TEST_FILENAME_LIST_PATH=%BASE_PATH%data\preprocessed\debug\training_data_list.txt
 set FEATURE_NAME_LIST_PATH=%BASE_PATH%data\preprocessed\debug\feature_name_list.txt
 ::set INPUT_ATLAS_DIR=%BASE_PATH%results\generate_atlas
 
 %EXE_PATH% %INPUT_FEATURE_DIR% %INPUT_ABD_MASK_DIR% %INPUT_PARAM_DIR% ^
             %INPUT_LABEL_DIR% %OUTPUT_DIR% %TEST_FILENAME_LIST_PATH% ^
-            %FEATURE_NAME_LIST_PATH% 
+            %FEATURE_NAME_LIST_PATH%
 
 PAUSE
 exit
